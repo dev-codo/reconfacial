869b83db96cfeb41ac782f4969af1501af4c92e9
dev-codo <dev.codo@gmail.com>
2018-06-05 23:36:44 -0400

    Img Recogn API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Particles from 'react-particles-js'; //background interactive
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+ apiKey: 'a5b7e6142ead4be8be340389abe45e70'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}                
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+  
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+        function(response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function(err) {
+        }
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          naMudancaInput={this.onInputChange} 
+          noBotaoSubmeter={ this.onButtonSubmit }/>
+        <FaceRecognition imageUrl={ this.state.imageUrl }/>
       </div>
     );
   }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+				{'Mingau....'}
+			</div>
+			<div className='white f1'>
+				{'Tomtom....'}
+			</div>
+		</div>
+	);
+};
+
+export default Rank;
